56a564f1693324f71f573fccf2d1e6e404a93b5b
Kalyaniaws <151449143+Kalyaniaws@users.noreply.github.com>
2023-12-02 20:04:14 +0530

    Update HelloController.java

--- src/main/java/com/example/mavendemo/HelloController.java
+++ src/main/java/com/example/mavendemo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "logged in succesfully-kalyani";
+        return "Succesful-Greetings kalyani";
     }
 
 }
